fbb834beb611b9e3093cca823e2ae4a834cae750
Peder Goodman <peder.goodman@gmail.com>
2023-07-19 14:33:12 -0500

    lecture notes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,34 +3,61 @@ import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
 function App() {
+// Sidebar
+
+// Generator functions
+
+// can pause them in the middle while keeping scope intact
+// can run multiple times, but will remember where it was...
+// paused in the middle once or many times, and resumed later
+function* myGenerator() {
+  // yield is a "soft return"
+  yield 1; 
+  yield 'hello';
+  yield {name: 'Peder'};
+  yield 35;
+};
+
+// create an instance of hte function
+const myGeneratorInstance = myGenerator();
+
+// execute the generator function and get back whatever was yield
+// console.log('first time:', myGeneratorInstance.next().value);
+// console.log('second time:', myGeneratorInstance.next().value);
+// console.log('third time:', myGeneratorInstance.next().value);
+// console.log('fourth time:', myGeneratorInstance.next().value);
+
+
+
+
+
+
+
   const dispatch = useDispatch();
   const elements = useSelector(store => store.elementList)
   const [newElement, setNewElement] = useState('');
 
-  const getElements = () => {
-    axios.get('/api/element').then(response => {
-      dispatch({ type: 'SET_ELEMENTS', payload: response.data });
-    })
-      .catch(error => {
-        console.log('error with element get request', error);
-      });
-  }
+
 
   useEffect(() => {
-    getElements();
+    dispatch({type: 'FETCH_ELEMENTS'})
   }, []);
 
   const addElement = () => {
-    axios.post('/api/element', { 
-      name: newElement
-    })
-      .then(() => {
-        getElements();
-        setNewElement('');
-      })
-      .catch(error => {
-        console.log('error with element get request', error);
-      });
+    dispatch({type: 'ADD_ELEMENT', payload: newElement});
+    setNewElement('');
+
+    
+    // axios.post('/api/element', { 
+    //   name: newElement
+    // })
+    //   .then(() => {
+    //     dispatch({type: 'FETCH_ELEMENTS'})
+    
+    //   })
+    //   .catch(error => {
+    //     console.log('error with element get request', error);
+    //   });
 
   }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+import { takeLatest, put } from 'redux-saga/effects'
+import axios from 'axios';
+
 
 const elementList = (state = [], action) => {
     switch (action.type) {
@@ -13,12 +16,60 @@ const elementList = (state = [], action) => {
         default:
             return state;
     }
-};    
+};
+
 
 // this is the saga that will watch for actions
 function* rootSaga() {
+    // listen for an action, then do something when that action is dispatched
+    // when the FETCH_ELEMENTS action is dispatched, run fetchElements
+    yield takeLatest('FETCH_ELEMENTS', fetchElements)
+    yield takeLatest('ADD_ELEMENT', addElement)
 
 }
+
+// we have access to the action that was dispatched
+function* fetchElements(action) {
+    console.log('fetch elements was dispatched with action:', action);
+
+    // historically we've done promise chaining (.then.catch) but now we
+    // don't need to do that because of yield generator magic
+
+    // try/catch: similar to a .then.catch, but with new syntax for sagas
+    // Do all the things in the try block, if there's an error catch it and 
+    // do whatever's in the catch block
+    try {
+        const elementResponse = yield axios.get('api/element');
+        // dispatch an action to update the redux store
+        // 'put' is the same thing as 'dispatch' in our React components!
+        // we can't use 'dispatch' because we get dispatch from useDispatch,
+        // which is a Reach-specific thing
+        yield put({ type: 'SET_ELEMENTS', payload: elementResponse.data })
+
+    } catch (error) {
+        console.log('error fetching elements:', error);
+    };
+
+
+
+}
+
+// add element saga
+// we have access to the action that was dispatched
+function* addElement(action) {
+    // make a post request!
+    try {
+        yield axios.post('/api/element', {
+            name: action.payload
+        })
+    // dispatch the FETCH_ELEMENTS action to trigger a GET request and get all the elements.
+        yield put({type: 'FETCH_ELEMENTS'})
+
+    } catch (error) {
+        console.log('error with post element:', error);
+    }
+}
+
 
 
 const sagaMiddleware = createSagaMiddleware();
